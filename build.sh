aa748501b181175ef79caa859243d4780135c6b8
Wink Saville <wink@saville.com>
2019-09-11 10:26:42 -0700

    Initial commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+# Protobuf
+(cd Joy && ./build.sh --go_out=../Go/src/ --javalite_out=../JoyJava/source/)
+(cd Perspective && ./build.sh --go_out=../Go/src/ --javalite_out=../PerspectiveJava/source/)
+
+# Java
+(cd AletheiaWareCommonJava && ./build.sh)
+(cd JoyJava && ./build.sh)
+(cd PerspectiveJava && ./build.sh)
