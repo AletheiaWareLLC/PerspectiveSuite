18426092b1c648cabba0e3fb9ab519971c2a7744
Stuart Scott <stuart.murray.scott@gmail.com>
2019-09-24 06:31:38 -0700

    Modify Scripts
    - Adds hash-bang bash
    - Adds copyright
    - Uses 'e' flag to exit on error
    - Uses 'x' flag to echo command being executed
    - Removes Go stuff

--- build.sh
+++ build.sh
@@ -1,9 +1,27 @@
+#!/bin/bash
+#
+# Copyright 2019 Aletheia Ware LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+#set -x
+
 # Protobuf
-mkdir -p Go/src
 echo "*** Build Joy"
-(cd Joy && ./build.sh --go_out=../Go/src/ --java_out=lite:../JoyJava/source/)
-echo "*** Build build Perspective"
-(cd Perspective && ./build.sh --go_out=../Go/src/ --java_out=lite:../PerspectiveJava/source/)
+(cd Joy && ./build.sh --java_out=lite:../JoyJava/source/)
+echo "*** Build Perspective"
+(cd Perspective && ./build.sh --java_out=lite:../PerspectiveJava/source/)
 
 # Java
 echo "*** Build AletheiaWareCommonJava"
@@ -13,7 +31,7 @@ echo "*** Build JoyJava"
 echo "*** Build PerspectiveJava"
 (cd PerspectiveJava && ./build.sh)
 
-# Gradle based builds
+# Gradle-based builds
 echo "*** Build AletheiaWareCommonAndroid"
 (cd AletheiaWareCommonAndroid && ./gradlew build)
 echo "*** Build JoyAndroid"
